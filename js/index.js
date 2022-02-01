d589d8990933bcc3518b03cfcbaef83a19b316aa
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-02-01 08:16:57 +0530

    changed the cart button

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-// Checking wether the user id is saved in cookies i.e it is checking is the logined earlier
-if (!document.cookie) {
-  location.href = "./pages/login.html";
-}
 // nav bar JS for opening menu in mobile view
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
 document.getElementById("h1").innerText =
   "The ID is " + document.cookie.replace("userid=", "");
+
+console.log(document.getElementsByName("accountdetails"));
+
+if (document.cookie.includes("userid")) {
+  $(".accountdetails").css("display", "block");
+  $(".signinbtn").css("display", "none");
+} else {
+  $(".accountdetails").css("display", "none");
+  $(".signinbtn").css("display", "block");
+}
