959d2bb2b0fe67fa9e9098475274984c622f13bd
Atharva Patil <65209968+atharva-p@users.noreply.github.com>
2022-02-02 10:17:49 +0530

    updated account page with index

--- js/accountdetails.js
+++ js/accountdetails.js
@@ -2,12 +2,15 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+document.getElementById("h1").innerText =
+  "The ID is " + document.cookie.replace("userid=", "");
 //for user authentication
 if (document.cookie.includes("userid")) {
   $(".accountdetails").css("display", "block");
   $(".signinbtn").css("display", "none");
 
-  $.getJSON("./json/customer-details.json", function (data) {
+  $.getJSON("../json/customer-details.json", function (data) {
     for (let i = 0; i < data.length; i++) {
       if (data[i].id.toString() === document.cookie.replace("userid=", "")) {
         document.getElementById("cart-items").innerText = data[i].cart.length;
