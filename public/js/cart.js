5e935723a27db874f4713edeed48a5ac069ad99f
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-02-10 18:30:11 +0530

    Cart Validation DONE

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,6 +3,23 @@ function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
 
+const RecieveData = async (uri) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      send: false,
+      data: undefined,
+    }),
+  };
+
+  const response = await fetch(uri, options);
+  const data = await response.json();
+  return data;
+};
+
 //for user authentication
 if (document.cookie.includes("userid")) {
   $(".accountdetails").css("display", "block");
@@ -16,14 +33,22 @@ if (document.cookie.includes("userid")) {
     cookieUserId = cookies[0];
   }
 
-  for (let i = 0; i < customerDetails.length; i++) {
-    if (
-      customerDetails[i].id.toString() === cookieUserId.replace("userid=", "")
-    ) {
-      document.getElementById("cart-items").innerText =
-        customerDetails[i].cart.length;
+  RecieveData("/customers").then((data) => {
+    let allCustomers = data;
+    for (let i = 0; i < allCustomers.length; i++) {
+      if (
+        allCustomers[i].id.toString() === cookieUserId.replace("userid=", "")
+      ) {
+        if (allCustomers[i].cart.length == 0) {
+          $(".cart-table").html(
+            "<div class='alert bg-danger text-light'><p>No items in your cart</p></div>"
+          );
+        }
+        document.getElementById("cart-items").innerText =
+          allCustomers[i].cart.length;
+      }
     }
-  }
+  });
 } else {
   $(".accountdetails").css("display", "none");
   $(".signinbtn").css("display", "block");
@@ -32,4 +57,7 @@ if (document.cookie.includes("userid")) {
 document.getElementById("signout").onclick = function () {
   document.cookie = "userid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   location.href = "../index.html";
+};
+document.getElementById("cart").onclick = function () {
+  location.href = "../pages/cart.html";
 };
